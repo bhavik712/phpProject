67352cb3d00fabfb3030dd52c8d77e7d3663105d
bhavikb <bhavikb@weblight.co.in>
2023-09-12 19:58:23 +0530

    Set Up of Project and use of partials

---
+++ views/about.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100">
+  <head>
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body class="h-full">
+    <div class="min-h-full">
+    
+      <?php require('partials/nav.php') ?>
+      <?php require('partials/header.php') ?>
+     
+      <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <p>About</p>
+        </div>
+      </main>
+    </div>
+
+  </body>
+</html>
